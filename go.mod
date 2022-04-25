126081b07f925ec284735b1cee98ca1273a64cbf
Jan Sang <hendrijsang@gmail.com>
2022-04-26 02:22:00 +0700

    menambah fungsi SayHello dengan parameter

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/hendrijs/go-say-hello
+module github.com/hendrijs/go-say-hello/v2
 
 go 1.18
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello dari say_hello.go"
+func SayHello(name string) string {
+	return "Hello" + name
 }
